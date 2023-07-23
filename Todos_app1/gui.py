e1321b3f8f8cef6f87c9dec7c1f0ad9b613556cf
Himanshu Vasani <himanshuvasani32@gmail.com>
2023-07-23 17:18:39 +0530

    Made add button operational

--- Todos_app1/gui.py
+++ Todos_app1/gui.py
@@ -2,9 +2,24 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Add a new to-do here")
-input_box = sg.InputText(tooltip="Enter a to-do")
+input_box = sg.InputText(tooltip="Enter a to-do", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window("To-do app", layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window("To-do app",
+                   layout=[[label], [input_box, add_button]],
+                   font=("Helvetica", 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todos = values["todo"] + "\n"
+            todos.append(new_todos)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
