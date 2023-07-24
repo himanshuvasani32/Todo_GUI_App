32afc91adfaf8c1be05ad16accd80b86721bec84
Himanshu Vasani <himanshuvasani32@gmail.com>
2023-07-24 10:19:57 +0530

    Completed feet. inches to meters converter

---
+++ Feet_to_Inches_Converter/converter_function.py
@@ -0,0 +1,3 @@
+def feet_inches_converter(feet, inches):
+    meters = feet * 0.3048 + inches * 0.0254
+    return meters
--- Feet_to_Inches_Converter/feet_to_inches_converter.py
+++ Feet_to_Inches_Converter/feet_to_inches_converter.py
@@ -1,15 +1,25 @@
 import PySimpleGUI as sg
+from converter_function import feet_inches_converter
 
 feet_label = sg.Text("Enter feet:")
-feet_input = sg.Input()
+feet_input = sg.Input(key="feet")
 
 inches_label = sg.Text("Enter inches:")
-inches_input = sg.Input()
+inches_input = sg.Input(key="inches")
 
 convert_button = sg.Button("Convert")
+meter_output = sg.Text("", key="output")
 
 window = sg.Window("Converter",
-                   layout=[[feet_label, feet_input], [inches_label, inches_input], [convert_button]])
+                   layout=[[feet_label, feet_input],
+                           [inches_label, inches_input],
+                           [convert_button, meter_output]])
 
-window.read()
+while True:
+    event, values = window.read()
+    feet = float(values["feet"])
+    inches = float(values["inches"])
+    result = feet_inches_converter(feet, inches)
+    window["output"].update(value=f"{feet} feet and {inches} inches is equal to {result} meters.")
+
 window.close()
