bfc80f770049d300f4a9b79508af3eff81460d6a
Himanshu Vasani <himanshuvasani32@gmail.com>
2023-07-24 22:39:51 +0530

    Added a theme and Exit button

--- Feet_to_Inches_Converter/feet_to_inches_converter.py
+++ Feet_to_Inches_Converter/feet_to_inches_converter.py
@@ -1,6 +1,8 @@
 import PySimpleGUI as sg
 from converter_function import feet_inches_converter
 
+theme = sg.theme('Black')
+
 feet_label = sg.Text("Enter feet:")
 feet_input = sg.Input(key="feet")
 
@@ -8,18 +10,24 @@ inches_label = sg.Text("Enter inches:")
 inches_input = sg.Input(key="inches")
 
 convert_button = sg.Button("Convert")
+exit_button = sg.Button("Exit")
 meter_output = sg.Text("", key="output")
 
 window = sg.Window("Converter",
                    layout=[[feet_label, feet_input],
                            [inches_label, inches_input],
-                           [convert_button, meter_output]])
+                           [convert_button, exit_button, meter_output]],
+                   font=('Helvetica', 20))
 
 while True:
     event, values = window.read()
-    feet = float(values["feet"])
-    inches = float(values["inches"])
-    result = feet_inches_converter(feet, inches)
-    window["output"].update(value=f"{feet} feet and {inches} inches is equal to {result} meters.")
+    match event:
+        case 'Convert':
+            feet = float(values["feet"])
+            inches = float(values["inches"])
+            result = feet_inches_converter(feet, inches)
+            window["output"].update(value=f"{feet} feet and {inches} inches is equal to {result} meters.")
+        case 'Exit':
+            break
 
 window.close()
